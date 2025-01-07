363c6b297859f5870a4e2941aedb590621680a85
ankit-1999-sinha <ankitecesinha@gmail.com>
2025-01-07 09:16:17 +0530

    first commit

---
+++ src/main/java/com/springboot/kafka/ApacheKafkaWithSpringBoot/ApacheKafkaWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.kafka.ApacheKafkaWithSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheKafkaWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheKafkaWithSpringBootApplication.class, args);
+	}
+
+}
